04dfc1ad3d43298a409a933298d5529c4a61141a
Frederic Hübner <frederic.huebner@sda.se>
2024-04-12 09:31:24 +0200

    Remove helm plugin

--- src/test/java/my/demo/groud/GreetingResourceIT.java
+++ src/test/java/my/demo/groud/GreetingResourceIT.java
@@ -1,10 +1,7 @@
 package my.demo.groud;
 
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.is;
 
 import io.quarkus.test.junit.QuarkusIntegrationTest;
-import org.junit.jupiter.api.Test;
 
 @QuarkusIntegrationTest
 class GreetingResourceIT extends GreetingResourceTest {
